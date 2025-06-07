1468b33a4ace2048cdc1ad0627e7e5c4937ad650
Yan Cerqueira <yansilvacerqueira@outlook.com>
2025-06-07 16:45:00 -0300

    creating funcs and variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+fn add_number(num: u32) -> u32 {
+    return 5 + num
+}
+
+// not using return keyword
+fn add_number_two(num: u32) -> u32 {
+    5 + num
+}
+
 fn main() {
+    let mut x = 32;
     println!("Hello, world!");
+    println!("x is {}", x);
+
+    x = add_number(x);
+    println!("now, x is {}", x);
+
+    x = add_number_two(x);
+    println!("x2 is {}", x);
+    
 }
